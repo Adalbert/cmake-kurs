4ab6cbaca04411de30e9ee33b087c4643c065437
Adalbert Soborka <a.soborka@eks-gmbh.com>
2022-10-18 09:13:07 +0200

    init commit

---
+++ 1helloworld/main.cpp
@@ -0,0 +1,8 @@
+//#include <iostream>
+#include "mylib.h"
+
+int main()
+{
+   printHW();
+   return 0;
+}
---
+++ 1helloworld/mylib.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "mylib.h"
+
+
+void printHW() {
+   std::cout << "Hallo swiat!" << std::endl;
+}
